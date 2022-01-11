1a7426e670a009b6c71c9269dc7be94a498047c2
Sehyuk Ahn <shyukahn@gmail.com>
2022-01-11 18:13:24 +0900

    added pepper (food)

--- src/main/kotlin/com/shyukahn/tutorial/item/ModItems.kt
+++ src/main/kotlin/com/shyukahn/tutorial/item/ModItems.kt
@@ -2,6 +2,7 @@ package com.shyukahn.tutorial.item
 
 import com.shyukahn.tutorial.Tutorial
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings
+import net.minecraft.item.FoodComponent
 import net.minecraft.item.Item
 import net.minecraft.util.Identifier
 import net.minecraft.util.registry.Registry
@@ -9,6 +10,9 @@ import net.minecraft.util.registry.Registry
 object ModItems {
 
     val RUBY = Item(FabricItemSettings().group(ModItemGroup.RUBY))
+    val PEPPER = Item(FabricItemSettings()
+        .food(FoodComponent.Builder().hunger(2).saturationModifier(0.2f).build())
+        .group(ModItemGroup.RUBY))
 
     private fun registerItem(name: String, item: Item) {
         Registry.register(Registry.ITEM, Identifier(Tutorial.MOD_ID, name), item)
@@ -18,5 +22,6 @@ object ModItems {
         println("Registering Mod Items for " + Tutorial.MOD_ID)
 
         registerItem("ruby", RUBY)
+        registerItem("pepper", PEPPER)
     }
 }
