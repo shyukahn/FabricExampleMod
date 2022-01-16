1e56d470bccec25f5b89d1fdbfd9aedcda7f47de
Sehyuk Ahn <shyukahn@gmail.com>
2022-01-16 17:08:51 +0900

    modified code of items and blocks

--- src/main/kotlin/com/shyukahn/tutorial/block/ModBlocks.kt
+++ src/main/kotlin/com/shyukahn/tutorial/block/ModBlocks.kt
@@ -6,40 +6,41 @@ import com.shyukahn.tutorial.block.custom.StatusBlock
 import com.shyukahn.tutorial.item.ModItemGroup
 import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings
-import net.minecraft.block.Block
-import net.minecraft.block.Material
-import net.minecraft.block.SlabBlock
+import net.minecraft.block.*
 import net.minecraft.item.BlockItem
+import net.minecraft.item.Item
 import net.minecraft.util.Identifier
 import net.minecraft.util.registry.Registry
 
 object ModBlocks {
 
-    val RUBY_ORE = Block(FabricBlockSettings.of(Material.STONE).strength(4.0f).requiresTool())
-    val RUBY_BLOCK = Block(FabricBlockSettings.of(Material.STONE).strength(6.0f).requiresTool())
-    val RUBY_STAIRS = ModStairsBlock(RUBY_BLOCK.defaultState,
-        FabricBlockSettings.of(Material.STONE).strength(6.0f).requiresTool())
-    val RUBY_SLAB = SlabBlock(FabricBlockSettings.of(Material.STONE).strength(6.0f).requiresTool())
+    val RUBY_ORE = registerBlock("ruby_ore",
+        Block(FabricBlockSettings.of(Material.STONE).strength(4.0f).requiresTool()))
 
-    val STATUS_BLOCK = StatusBlock(FabricBlockSettings.of(Material.STONE).strength(6.0f).requiresTool())
+    val RUBY_BLOCK = registerBlock("ruby_block",
+        Block(FabricBlockSettings.of(Material.STONE).strength(6.0f).requiresTool()))
 
-    private fun registerBlock(name: String, block: Block) {
+    val RUBY_STAIRS = registerBlock("ruby_stairs",
+        ModStairsBlock(RUBY_BLOCK.defaultState,
+            FabricBlockSettings.of(Material.STONE).strength(6.0f).requiresTool()))
+
+    val RUBY_SLAB = registerBlock("ruby_slab",
+        SlabBlock(FabricBlockSettings.of(Material.STONE).strength(6.0f).requiresTool()))
+
+    val STATUS_BLOCK = registerBlock("status_block",
+        StatusBlock(FabricBlockSettings.of(Material.STONE).strength(6.0f).requiresTool()))
+
+    private fun registerBlock(name: String, block: Block): Block {
         registerBlockItem(name, block)
-        Registry.register(Registry.BLOCK, Identifier(Tutorial.MOD_ID, name), block)
+        return Registry.register(Registry.BLOCK, Identifier(Tutorial.MOD_ID, name), block)
     }
 
-    private fun registerBlockItem(name: String, block: Block) {
-        Registry.register(Registry.ITEM, Identifier(Tutorial.MOD_ID, name),
+    private fun registerBlockItem(name: String, block: Block): Item {
+        return Registry.register(Registry.ITEM, Identifier(Tutorial.MOD_ID, name),
             BlockItem(block, FabricItemSettings().group(ModItemGroup.RUBY)))
     }
 
     fun registerModBlocks() {
         println("Registering Mod Blocks for " + Tutorial.MOD_ID)
-
-        registerBlock("ruby_ore", RUBY_ORE)
-        registerBlock("ruby_block", RUBY_BLOCK)
-        registerBlock("ruby_stairs", RUBY_STAIRS)
-        registerBlock("ruby_slab", RUBY_SLAB)
-        registerBlock("status_block", STATUS_BLOCK)
     }
 }
--- src/main/kotlin/com/shyukahn/tutorial/item/ModItems.kt
+++ src/main/kotlin/com/shyukahn/tutorial/item/ModItems.kt
@@ -10,23 +10,24 @@ import net.minecraft.util.registry.Registry
 
 object ModItems {
 
-    val RUBY = Item(FabricItemSettings().group(ModItemGroup.RUBY))
-    val PEPPER = Item(FabricItemSettings()
-        .food(FoodComponent.Builder().hunger(2).saturationModifier(0.2f).build())
-        .group(ModItemGroup.RUBY))
-    val DOWSING_ROD = DowsingRodItem(FabricItemSettings().group(ModItemGroup.RUBY).maxDamage(10))
-    val IRON_WOOL = Item(FabricItemSettings().group(ModItemGroup.RUBY))
+    val RUBY = registerItem("ruby",
+        Item(FabricItemSettings().group(ModItemGroup.RUBY)))
 
-    private fun registerItem(name: String, item: Item) {
-        Registry.register(Registry.ITEM, Identifier(Tutorial.MOD_ID, name), item)
+    val PEPPER = registerItem("pepper", Item(FabricItemSettings()
+        .food(FoodComponent.Builder().hunger(2).saturationModifier(0.2f).build())
+        .group(ModItemGroup.RUBY)))
+
+    val DOWSING_ROD = registerItem("dowsing_rod",
+        DowsingRodItem(FabricItemSettings().group(ModItemGroup.RUBY).maxDamage(10)))
+
+    val IRON_WOOL = registerItem("iron_wool",
+        Item(FabricItemSettings().group(ModItemGroup.RUBY)))
+
+    private fun registerItem(name: String, item: Item): Item {
+        return Registry.register(Registry.ITEM, Identifier(Tutorial.MOD_ID, name), item)
     }
 
     fun registerModItems() {
         println("Registering Mod Items for " + Tutorial.MOD_ID)
-
-        registerItem("ruby", RUBY)
-        registerItem("pepper", PEPPER)
-        registerItem("dowsing_rod", DOWSING_ROD)
-        registerItem("iron_wool", IRON_WOOL)
     }
 }
