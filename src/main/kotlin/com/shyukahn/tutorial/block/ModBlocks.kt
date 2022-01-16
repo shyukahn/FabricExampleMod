fc53628121d0b17cc648fee1ed7df98bd45f7265
Sehyuk Ahn <shyukahn@gmail.com>
2022-01-16 16:49:32 +0900

    added ruby slabs and ruby stairs

--- src/main/kotlin/com/shyukahn/tutorial/block/ModBlocks.kt
+++ src/main/kotlin/com/shyukahn/tutorial/block/ModBlocks.kt
@@ -1,21 +1,27 @@
 package com.shyukahn.tutorial.block
 
 import com.shyukahn.tutorial.Tutorial
+import com.shyukahn.tutorial.block.custom.ModStairsBlock
 import com.shyukahn.tutorial.block.custom.StatusBlock
 import com.shyukahn.tutorial.item.ModItemGroup
 import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings
 import net.minecraft.block.Block
 import net.minecraft.block.Material
+import net.minecraft.block.SlabBlock
 import net.minecraft.item.BlockItem
 import net.minecraft.util.Identifier
 import net.minecraft.util.registry.Registry
 
 object ModBlocks {
 
-    private val RUBY_ORE = Block(FabricBlockSettings.of(Material.STONE).strength(4.0f).requiresTool())
-    private val RUBY_BLOCK = Block(FabricBlockSettings.of(Material.STONE).strength(6.0f).requiresTool())
-    private val STATUS_BLOCK = StatusBlock(FabricBlockSettings.of(Material.STONE).strength(6.0f).requiresTool())
+    val RUBY_ORE = Block(FabricBlockSettings.of(Material.STONE).strength(4.0f).requiresTool())
+    val RUBY_BLOCK = Block(FabricBlockSettings.of(Material.STONE).strength(6.0f).requiresTool())
+    val RUBY_STAIRS = ModStairsBlock(RUBY_BLOCK.defaultState,
+        FabricBlockSettings.of(Material.STONE).strength(6.0f).requiresTool())
+    val RUBY_SLAB = SlabBlock(FabricBlockSettings.of(Material.STONE).strength(6.0f).requiresTool())
+
+    val STATUS_BLOCK = StatusBlock(FabricBlockSettings.of(Material.STONE).strength(6.0f).requiresTool())
 
     private fun registerBlock(name: String, block: Block) {
         registerBlockItem(name, block)
@@ -32,6 +38,8 @@ object ModBlocks {
 
         registerBlock("ruby_ore", RUBY_ORE)
         registerBlock("ruby_block", RUBY_BLOCK)
+        registerBlock("ruby_stairs", RUBY_STAIRS)
+        registerBlock("ruby_slab", RUBY_SLAB)
         registerBlock("status_block", STATUS_BLOCK)
     }
 }
---
+++ src/main/kotlin/com/shyukahn/tutorial/block/custom/ModStairsBlock.kt
@@ -0,0 +1,6 @@
+package com.shyukahn.tutorial.block.custom
+
+import net.minecraft.block.BlockState
+import net.minecraft.block.StairsBlock
+
+class ModStairsBlock(baseBlockState: BlockState, settings: Settings) : StairsBlock(baseBlockState, settings)
